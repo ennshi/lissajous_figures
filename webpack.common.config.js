10c677a4461e73f99ae8436fd90cfa4c8722b071
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-11-27 14:13:50 +0100

    Initial settings npm, webpack

---
+++ webpack.common.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    entry: {
+        main: "./src/index.js",
+        vendor: "./src/vendor.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html/,
+                use: [
+                    "html-loader"
+                ]
+            }
+        ]
+    }
+};

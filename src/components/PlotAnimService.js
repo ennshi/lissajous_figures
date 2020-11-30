d74378a2bca98d808df3027a575ba3902c233fb9
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-11-30 18:05:16 +0100

    PlotAnimService created

---
+++ src/components/PlotAnimService.js
@@ -0,0 +1,23 @@
+
+export default (function () {
+    const params = {
+        start: false,
+        reqFrame: null,
+        numPlots: 0
+    };
+    const setDefaultPlot = () => {
+
+    };
+    const startAnimation = () => {
+
+    };
+    const stopAnimation = () => {
+
+    };
+    const addPlot = () => {
+
+    };
+    const clearAll = () => {
+
+    };
+})();

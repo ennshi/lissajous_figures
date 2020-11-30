e362000a27973c895f6d5faabed56c8ee049dc7b
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-11-30 13:18:17 +0100

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const AMPLITUDE = 1;
+export const MIN_FREQUENCY = 1;
+export const MAX_FREQUENCY = 15;
+export const dT = 0.01;
+export const MAX_T = 800;
+export const SPEED_dPHASE = {
+    'X2': 0.001,
+    'X4': 0.002,
+    'X8': 0.004,
+    'X1': 0.008
+};
+
+export const MAX_PHASE = 2.000;
+

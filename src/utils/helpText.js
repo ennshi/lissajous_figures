2550bbe5b09cec8f50c2575254d73e26d3fc87af
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-12-01 18:33:51 +0100

    helpText created

---
+++ src/utils/helpText.js
@@ -0,0 +1,8 @@
+export const helpText = `<ul>
+    <li>Lissajous figures are plane curves traced by a point which executes two independent harmonic motions in perpendicular directions, the frequencies of the motion being in the ratio of two integers.</li>
+    <li>The mini-project simulates Lissajous figures depending on frequency ratio (no real scientific approach, just drawing). The project is built with JS, plotly.js, webpack.</li>
+    <li>The animation displays transformation of figure depending on the phase difference changing in time.</li>
+    <li>Set frequencies of X and Y motions using arrow buttons.</li>
+    <li>Change speed with the 'X2' button.</li>
+    <li>You can save and compare curves with Save button.</li>
+</ul>`;
